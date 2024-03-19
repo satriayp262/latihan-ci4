be9d5a49248e1f92b84646be7e04782d7b273014
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2024-03-19 19:36:38 +0700

    progress

--- app/Views/pegawai/index.php
+++ app/Views/pegawai/index.php
@@ -8,7 +8,7 @@
         <div class="card-body">
             <?php if (!empty(session()->getFlashdata('message'))) : ?>
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    <?php echo session()->getFlashdata('message'); ?>
+                    <?= session()->getFlashdata('message'); ?>
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
